e1bb8090172009678194c3e8951b165630276de1
Zachary Goldstein <zgoldstein@cmu.edu>
2021-08-28 17:14:02 -0400

    remove analysis files and update bot message

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ bot = commands.Bot(command_prefix='!', help_command=None)
 async def whenrating(ctx, name, rating, variant="Rapid"):
   error_msg, prob_success, predicted_date = process_inputs(name,rating,variant)
   if error_msg == 'No error':
-    msg = f"{name} {rating} {variant.title()}: {prob_success} chance within 2 years."
-    if int(prob_success[:-1]) > 5: msg += f" If user succeeds, expected date is {predicted_date}."
+    msg = f"{name}'s chance of reaching {rating} {variant.title()} within 2 years: **{prob_success}**"
+    if int(prob_success[:-1]) > 5: msg += f"\nExpected date: **{predicted_date}**."
     await ctx.send(msg)  
   else:
     await ctx.send(error_msg)
